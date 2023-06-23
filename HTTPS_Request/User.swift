9f3722785b885dc46fc5d9239ebce0dd698ed59b
Caio Vinicius <viniciuscaioml@gmail.com>
2023-06-23 20:37:29 -0300

    HTTP Request APP

---
+++ HTTPS_Request/User.swift
@@ -0,0 +1,38 @@
+//
+//  User.swift
+//  HTTPS_Request
+//
+//  Created by Caio Vinicius on 23/06/23.
+//
+
+import Foundation
+
+struct User: Identifiable, Decodable {
+    var id: Int
+    var name: String
+    var username: String
+    var email: String
+    var address: Address
+    var phone: String
+    var website: String
+    var company: Company
+
+    struct Address: Decodable {
+        var street: String
+        var suite: String
+        var city: String
+        var zipcode: String
+        var geo: Geo
+
+        struct Geo: Decodable {
+            var lat: String
+            var lng: String
+        }
+    }
+
+    struct Company: Decodable {
+        var name: String
+        var catchPhrase: String
+        var bs: String
+    }
+}
